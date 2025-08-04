a127efdcf707b338baf709648bc98699e066db24
leonardo-lim <leonardolim178@gmail.com>
2025-08-04 23:50:40 +0700

    feat: add utility to fetch token info

---
+++ src/utils/token-info.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const getTokenInfo = async (address: string) => {
+    const response = await axios.get(`https://api.dexscreener.com/latest/dex/tokens/${address}`);
+    const data = response.data.pairs[0];
+
+    return {
+        name: data.baseToken.name,
+        chain: data.chainId,
+        price: data.priceUsd,
+        marketCap: data.marketCap,
+        fdv: data.fdv,
+        volume24h: data.volume.h24,
+        liquidity: data.liquidity.usd
+    };
+};
+
+export { getTokenInfo };

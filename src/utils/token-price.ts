46e6a05588aca1e42b43bf9e13d020d7a0c70f6d
leonardo-lim <leonardolim178@gmail.com>
2025-08-05 14:37:58 +0700

    feat: add utility to fetch token price

---
+++ src/utils/token-price.ts
@@ -0,0 +1,38 @@
+import type { DexPair } from '../types/dex-pair-type';
+import axios from 'axios';
+
+const getSearchPairs = async (tokenName: string) => {
+    const response = await axios.get(`https://api.dexscreener.com/latest/dex/search?q=${tokenName}`);
+    return response.data.pairs;
+};
+
+const getTokenPrice = async (tokenName: string, chainName: string | null) => {
+    const searchPairs: DexPair[] = await getSearchPairs(tokenName);
+    let pair: DexPair | undefined = undefined;
+
+    if (chainName) {
+        pair = searchPairs.find((pair) => (
+            pair.baseToken.symbol.toLowerCase() === tokenName.toLowerCase() &&
+            pair.chainId === chainName
+        ));
+    } else {
+        pair = searchPairs.find((pair) => (
+            pair.baseToken.symbol.toLowerCase() === tokenName.toLowerCase()
+        ));
+    }
+
+    const { chainId, pairAddress } = pair!;
+
+    const response = await axios.get(`https://api.dexscreener.com/latest/dex/pairs/${chainId}/${pairAddress}`);
+    const data = response.data.pairs[0];
+
+    return {
+        name: data.baseToken.name,
+        chain: data.chainId,
+        price: data.priceUsd,
+        volume24h: data.volume.h24,
+        liquidity: data.liquidity.usd
+    };
+};
+
+export { getTokenPrice };
